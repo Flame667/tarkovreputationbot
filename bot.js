4b6945e9cf273fc4f534f5afc853894655fcb8fb
cranberriez <jakevilevac@gmail.com>
2018-04-24 16:01:17 -0700

    Updated Deprecated Command

--- bot.js
+++ bot.js
@@ -18,17 +18,17 @@ var raffleentries = [];
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setGame(`on ${client.guilds.size} servers`)
+  client.user.setActivity(`on ${client.guilds.size} servers`)
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
 function timedRoles(message) {
